f3d70fc94ad2337de68285d985d27538e1957515
Hard-Pressed <robertpreston.lwtc@yahoo.com>
2025-08-26 19:24:38 -0700

    D: Added Delta time

--- main.py
+++ main.py
@@ -6,18 +6,27 @@ from constants import *
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+clock = pygame.time.Clock()
+dt = 0
+
+
 def main():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    global dt
     # Main game loop
     running = True
     while running:
+        # Delta time
+        dt = clock.tick(60) / 1000
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
 
+        # Black screen
         screen.fill((0, 0, 0))
         pygame.display.flip()
 
